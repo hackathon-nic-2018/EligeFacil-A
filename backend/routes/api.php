b992330a242bd1a4c6cdff32b6a58f566d677ce3
ddelagneau <daviddelagneau@yahoo.es>
2018-11-30 08:33:54 -0600

    Rutas para angular

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,7 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => 'cors'], function() {
+   Route::resource('product','ProductController');
+   Route::resource('store','StoreController');
+   Route::resource('user','UserController');
+   Route::get('search/{busqueda}',function($busqueda){
+     $products = App\Product::all();
 
-Route::resource('product','ProductController');
-Route::resource('store','StoreController');
-Route::resource('user','UserController');
