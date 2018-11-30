be6cc84cca544cc7e0283ccb2150e79fe70878ad
ddelagneau <daviddelagneau@yahoo.es>
2018-11-30 08:32:18 -0600

    Imagen de rpoducto

--- backend/app/Product.php
+++ backend/app/Product.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -12,7 +13,26 @@ class Product extends Model
       'precio'
     ];
 
+    protected $appends = [
+      'tienda',
+    ];
+    public function getTiendaAttribute(){
+        return $this->tiendas->nombre;
+    }
     public function imagen(){
       return $this->belongsToMany('App\Image');
     }
+
+    public function tiendas(){
+      return $this->belongsTo('App\Store','store_id');
+    }
+
+    public function getImagenAttribute(){
+      // if($this->ImagenProducto){
+      //   if(!Storage::exists('public\img\producto-'.$this->IdProductos.'.jpg')){
+      //     Storage::put('public\img\producto-'.$this->IdProductos.'.jpg', $this->ImagenProducto);
+      //     // Image::make($producto->ImagenProducto)->save('storage/imagenGenerada.jpg')
+      //   }
+        return '.'.Storage::url('img/producto-'.$this->id.'.jpg');
+      }
 }
